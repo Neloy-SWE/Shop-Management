77ff570f98a90fa6152e94a1434c7d24cbec7d4e
Neloy-SWE <taufiqneloy.swe@gmail.com>
2022-10-24 19:45:31 +0600

    grid view fixed height component added

--- lib/screens/screen_homepage.dart
+++ lib/screens/screen_homepage.dart
@@ -78,6 +78,9 @@ class _HomePageState extends State<HomePage> {
               btnText: AllTexts.addNewShop,
               onTap: () {},
             ),
+
+            // shop list
+            // GridView.builder(gridDelegate: gridDelegate, itemBuilder: itemBuilder)
           ],
         ),
       ),
